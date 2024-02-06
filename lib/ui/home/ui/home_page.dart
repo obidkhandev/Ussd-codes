25b3888dce10fe86f05e74ddfd829c1214b18c27
@alwaysflutter <obidkhandev@gmail.com>
2024-02-06 21:47:35 +0500

    carusel

--- lib/ui/home/ui/home_page.dart
+++ lib/ui/home/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:ussd_code/ui/home/ui/widget/carusel_widget.dart';
 import 'package:ussd_code/utils/constants/app_images.dart';
 import 'package:ussd_code/utils/tools/file_importers.dart';
 
@@ -28,35 +29,17 @@ class HomePage extends StatelessWidget {
                   child: TabBarView(
                     children: [
                       Container(
-                        height: height(context) * 0.8,
+                        height: height(context) * 0.9,
                         // color: Colors.red,
                         // child: Text("Mobiuz"),
                         child: Column(
                           children: [
-                            FlutterCarousel(
-                              options: CarouselOptions(
-                                height: 400.0,
-                                showIndicator: true,
-                                slideIndicator: const CircularSlideIndicator(),
-                              ),
-                              items: [1, 2, 3, 4, 5].map((i) {
-                                return Builder(
-                                  builder: (BuildContext context) {
-                                    return Container(
-                                        width:
-                                            MediaQuery.of(context).size.width,
-                                        margin: EdgeInsets.symmetric(
-                                            horizontal: 5.0),
-                                        decoration:
-                                            BoxDecoration(color: Colors.amber),
-                                        child: Text(
-                                          'text $i',
-                                          style: TextStyle(fontSize: 16.0),
-                                        ));
-                                  },
-                                );
-                              }).toList(),
-                            )
+                            CaruselWidget(
+                              images: [
+                                AppImage.mobiuzBanner1,
+                                AppImage.mobiuzBanner2,
+                              ],
+                            ),
                           ],
                         ),
                       ),
---
+++ lib/ui/home/ui/widget/carusel_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:ussd_code/utils/tools/file_importers.dart';
+
+class CaruselWidget extends StatelessWidget {
+  final List images;
+  const CaruselWidget({
+    required this.images,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterCarousel(
+      options: CarouselOptions(
+        height: height(context) * 0.3,
+        showIndicator: true,
+        autoPlay: true,
+        autoPlayAnimationDuration: const Duration(seconds: 3),
+        slideIndicator: const CircularSlideIndicator(),
+      ),
+      items: images.map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              margin: const EdgeInsets.all(6),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  image:
+                      DecorationImage(image: AssetImage(i), fit: BoxFit.cover),
+                  color: Colors.amber),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
